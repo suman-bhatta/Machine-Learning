6fdd7b7c6b7299e685e33ee0bcd6c1dc51d96890
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-11 17:54:46 +0545

    Add implementations for mean, median, and mode calculations

---
+++ Mean Median Mode/Mean.py
@@ -0,0 +1,13 @@
+"Mean"
+# mean = (sum of all data points) / (number of data points)
+
+import numpy as np
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = np.mean(speed)
+
+print(x)
+
+
+
---
+++ Mean Median Mode/Median.py
@@ -0,0 +1,9 @@
+"Median"
+
+import numpy 
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.median(speed)
+
+print(x)
