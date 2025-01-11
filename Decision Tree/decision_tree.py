bd64de7bde1562e90ef1087cf252a56dcfcb98b4
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-11 22:52:02 +0545

    Add decision tree implementation with data preprocessing and visualization

---
+++ Decision Tree/decision_tree.py
@@ -0,0 +1,69 @@
+"""Read and print the data set:"""
+
+# import pandas
+
+# df = pandas.read_csv("data.csv")
+
+# print(df)
+
+
+"Change string values into numerical values:"
+
+import pandas
+
+df = pandas.read_csv("data.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+print(df)
+
+
+
+"X is the feature columns, y is the target column:"
+
+import pandas
+
+df = pandas.read_csv("data.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+print(X)
+print(y)
+
+
+
+"Create and display a Decision Tree:"
+
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv("data.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+tree.plot_tree(dtree, feature_names=features)
+
