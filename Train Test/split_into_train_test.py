db918e2e6ccebd7cc2df9739429a51eda56e7bb7
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-11 22:41:26 +0545

    Add data preparation and model fitting scripts for regression analysis

---
+++ Train Test/split_into_train_test.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+np.random.seed(2)
+
+# Example data
+x = np.random.rand(100)
+y = np.random.rand(100)
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, random_state=2)
+
+plt.scatter(train_x, train_y)
+plt.show()
+
+
+" Display the Testing Set"
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+np.random.seed(2)
+
+# Example data
+x = np.random.rand(100)
+y = np.random.rand(100)
+
+
+plt.scatter(test_x, test_y)
+plt.show()
+
+
+" Fit the Data Set"
+
+import numpy
+import matplotlib.pyplot as plt
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+myline = numpy.linspace(0, 6, 100)
+
+plt.scatter(train_x, train_y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+
+
+
+"R2"
+
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(train_y, mymodel(train_x))
+
+print(r2)
+
+
+"Bring in the Testing Set"
+
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(test_y, mymodel(test_x))
+
+print(r2)
+
+
+"Predict Values"
+
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+print(mymodel(5))
