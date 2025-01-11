6674e0e091c58a4816eba330bfb1ccd2ea4b4dfc
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-11 18:19:35 +0545

    Add implementation for standard deviation and variance calculations

---
+++ Standard Deviation/calculate_standard_deviation.py
@@ -0,0 +1,30 @@
+"Standard Derivation"
+
+import numpy as np
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = np.std(speed)
+
+print(x)
+
+
+# Example
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
+
+
+"Variance"
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.var(speed)
+
+print(x)
